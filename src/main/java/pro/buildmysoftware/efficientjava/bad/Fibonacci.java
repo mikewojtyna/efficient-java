38c4c5028234d26b312c6388e20416dc6a29ba1a
Mike Wojtyna <mike@buildmysoftware.pro>
2019-01-06 21:52:05 +0100

    Add fibonacci bad example

---
+++ src/main/java/pro/buildmysoftware/efficientjava/bad/Fibonacci.java
@@ -0,0 +1,30 @@
+package pro.buildmysoftware.efficientjava.bad;
+
+class Fibonacci {
+	static int fibonacci(int n) {
+		return recursiveFibonacci(n);
+		// uncomment this
+		// return dynamicFibonacci(n);
+	}
+
+	private static int dynamicFibonacci(int n) {
+		if (n < 2) {
+			return n;
+		}
+		int current = 1;
+		int previous = 1;
+		for (int i = 2; i < n; i++) {
+			int oldPrevious = current;
+			current += previous;
+			previous = oldPrevious;
+		}
+		return current;
+	}
+
+	private static int recursiveFibonacci(int n) {
+		if (n < 2) {
+			return n;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
---
+++ src/test/java/pro/buildmysoftware/efficientjava/bad/FibonacciTest.java
@@ -0,0 +1,29 @@
+package pro.buildmysoftware.efficientjava.bad;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class FibonacciTest {
+	// @formatter:off
+	@DisplayName("should sum fibonacci numbers")
+	@CsvSource({
+		"0, 0",
+		"1, 1",
+		"2, 1",
+		"3, 2",
+		"4, 3",
+		"5, 5",
+		"6, 8",
+		"7, 13",
+		"8, 21"
+
+	})
+	// @formatter:on
+	@ParameterizedTest(name = "given: n={0}; expected: sum={1}")
+	void test(int n, int expectedSum) throws Exception {
+		assertThat(Fibonacci.fibonacci(n)).isEqualTo(expectedSum);
+	}
+}
