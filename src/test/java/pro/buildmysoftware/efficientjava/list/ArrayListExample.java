5bc4d3663a8e8853479d5b3ebdcca640527be229
Mike Wojtyna <mike@buildmysoftware.pro>
2019-01-09 08:39:17 +0100

    Add sublist view example

--- src/test/java/pro/buildmysoftware/efficientjava/list/ArrayListExample.java
+++ src/test/java/pro/buildmysoftware/efficientjava/list/ArrayListExample.java
@@ -5,11 +5,30 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class ArrayListExample {
+
+	@DisplayName("show how to use sublist views")
+	@Test
+	void test0() throws Exception {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		List<Integer> subList = numbers.subList(1, 3);
+		assertThat(subList).containsExactly(2, 3);
+
+		subList.remove(0);
+		assertThat(numbers).containsExactly(1, 3);
+	}
+
 	@DisplayName("show array lists usage")
 	@Test
-	void test() throws Exception {
+	void test1() throws Exception {
 		ArrayList<Integer> list = new ArrayList<>();
 		list.add(1);
 		list.add(2);
