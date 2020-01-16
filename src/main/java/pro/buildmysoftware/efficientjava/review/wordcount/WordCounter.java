21b095fe94f7f7d8fec998f76e8c80015919ff6e
Mike Wojtyna <mike@buildmysoftware.pro>
2020-01-16 01:52:00 +0100

    Fix word counter

--- src/main/java/pro/buildmysoftware/efficientjava/review/wordcount/WordCounter.java
+++ src/main/java/pro/buildmysoftware/efficientjava/review/wordcount/WordCounter.java
@@ -7,17 +7,19 @@ import java.util.Collection;
 import java.util.List;
 
 class WordCounter {
+
 	static Collection<Word> countWords(String text) {
 		List<String> wordsAsString = Arrays.asList(text.split("\\s"));
 		Collection<Word> words = new ArrayList<>();
 		for (String wordString : wordsAsString) {
-			boolean wordFoundPreviously = false;
+			boolean newWord = true;
 			for (Word word : words) {
 				if (wordString.equals(word.getValue())) {
+					newWord = false;
 					word.setTimes(word.getTimes() + 1);
 				}
 			}
-			if (!wordFoundPreviously) {
+			if (newWord) {
 				Word word = new Word();
 				word.setValue(wordString);
 				word.setTimes(1);
